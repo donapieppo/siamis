2f9f7b1af99266a5b540a5d074866e4f34703481
Pietro <donapieppo@yahoo.it>
2016-10-13 16:45:10 +0200

    pretender

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
     (@current_user ||= User.find(session[:user_id])) if session[:user_id]
   end
 
+  impersonates :user
+
   def log_current_user
     current_user or return true
     logger.info("Current user: #{current_user.email}")
---
+++ app/controllers/impersonations_controller.rb
@@ -0,0 +1,39 @@
+class ImpersonationsController < ApplicationController
+  before_action :force_sso_user
+
+  def who_impersonate
+    if true_user_can_impersonate?
+      @users = User.order(:surname)
+    else
+      redirect_to root_path and return 
+    end
+  end
+
+  def impersonate
+    if true_user_can_impersonate?
+      user = User.find(params[:id])
+      logger.info("IMPERSONATION: #{current_user.inspect} -> #{user.inspect}")
+      session[:new_impersonation] = true
+      impersonate_user(user)
+    end
+    redirect_to root_path and return
+  end
+
+  # do not require admin for this method if access control
+  # is performed on the current_user instead of true_user
+  def stop_impersonating
+    stop_impersonating_user
+    redirect_to root_path and return
+  end
+
+  private 
+
+  # for example in config/initializers/project_name.rb you have
+  # config.impersonate_admins = ['user.one@unibo.it', 'user.two@unibo.it']
+  def true_user_can_impersonate?
+    true_user and Rails.configuration.impersonate_admins and Rails.configuration.impersonate_admins.include?(true_user.email)
+  end
+end
+
+
+
