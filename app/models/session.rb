5fca77ae6f392bb6200d8ad225c30fcda9b9e6f2
Pietro <donapieppo@yahoo.it>
2016-10-21 16:33:32 +0200

    chair

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,9 +1,10 @@
 class Session < ApplicationRecord
   has_many :organizers, dependent: :destroy
+  has_one  :chair, class_name: User, foreign_key: :chair_id
   has_many :presentations # FIXME dependent
   has_many :ratings, dependent: :destroy
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   def to_s
     self.name
