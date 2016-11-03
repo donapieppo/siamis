8baca27f5fa5cbc0e0cfbb029e54a7044bdbfdfb
Pietro Donatini <pietro.donatini@unibo.it>
2016-11-03 13:50:32 +0100

    now we use devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :current_user_owns?, :current_user_owns!, :true_user_can_impersonate?, :user_committee_organizer?
+  helper_method :current_user_owns?, :current_user_owns!, :true_user_can_impersonate?, :user_committee_organizer?
 
-  before_action :log_current_user, :force_sso_user, :registration
-
-  def current_user
-    return nil unless session[:user_id]
-    (@current_user ||= User.find(session[:user_id])) if session[:user_id]
-  end
+  before_action :log_current_user, :authenticate_user!, :registration
 
   impersonates :user
 
   def log_current_user
     current_user or return true
     logger.info("Current user: #{current_user.email}")
-  end
-
-  # see Rails.configuration.dm_unibo_common[:omniauth_provider]
-  # actually: shibboleth (for unibo) and google_oauth2
-  # Use in app/controllers/application_controller.rb like
-  # before_filter :log_current_user, :force_sso_user
-  def force_sso_user
-    if ! current_user
-      conference_session[:original_request] = request.fullpath
-      redirect_to login_path and return 
-    end
   end
 
   def registration
@@ -72,7 +56,7 @@ class ApplicationController < ActionController::Base
     @minisymposium = Minisymposium.find(params[:minisymposium_id]) if params[:minisymposium_id]
     @minitutorial  = Minitutorial.find(params[:minitutorial_id])   if params[:minitutorial_id]
     @presentation  = Presentation.find(params[:presentation_id])   if params[:presentation_id]
-    @what = @minisymposium || @minitutorial || @presentation
+    @conference_session = @minisymposium || @minitutorial || @presentation
   end
 
 end
