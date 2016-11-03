a8ea9a1e509d70f62a0458d48e5f678771423d40
Pietro Donatini <pietro.donatini@unibo.it>
2016-11-03 13:53:36 +0100

    try registration mail

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,2 +1,9 @@
 class RegistrationMailer < ApplicationMailer
+  def welcome (user, password, for_what)
+    @user     = user
+    @password = password
+    @for_what = for_what
+    mail(to:      @user.email,
+         subject: "Welcome Siam-is18") 
+  end
 end
