c80e8f1219bda779185d843cd997f0e9f8779e18
Pietro Donatini <pietro.donatini@unibo.it>
2016-11-04 16:08:26 +0100

    conference_registration as devise brings registration for user

---
+++ app/controllers/conference_registrations_controller.rb
@@ -0,0 +1,17 @@
+class ConferenceRegistrationsController < ApplicationController
+  def new
+    @me  = current_user
+    @fee = Fee.new(@me)
+    @price_to_pay_and_reason = @fee.price_to_pay_and_reason
+  end
+
+  def show
+    @registrtion = current_user.registration
+    @payment = @registration.payment
+  end
+
+  def check
+    raise "OK"
+  end
+end
+
---
+++ app/models/conference_registration.rb
@@ -0,0 +1,5 @@
+class ConferenceRegistration < ApplicationRecord
+  belongs_to :user
+  belongs_to :payment
+
+end
