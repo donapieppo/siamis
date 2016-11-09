e7f29299bef7f053dd6820746b040cc6b7750d72
Pietro Donatini <pietro.donatini@unibo.it>
2016-11-09 11:00:02 +0100

    active account when inserted as author/organizer

--- app/models/concerns/role.rb
+++ app/models/concerns/role.rb
@@ -15,7 +15,7 @@ module Role
 
     # without email noting interesting here
     if self.email.blank?
-      self.errors.add(:email, 'Give email')  
+      self.errors.add(:email, 'Please enter email address')  
     elsif _user = User.where(email: self.email).first 
       if self.is_a?(Organizer) and self.conference_session_id and self.class.where(user_id: _user.id, conference_session_id: self.conference_session_id).any? 
         self.errors.add(:email, "User is already organizer of this #{self.conference_session.class}")
@@ -25,19 +25,20 @@ module Role
       self.user_id = _user.id 
     else
       if self.name.blank? 
-        self.errors.add(:name, "Please enter the name of the user") if self.name.blank?
+        self.errors.add(:name, "Please enter the name of the user") 
       elsif self.surname.blank? 
         self.errors.add(:surname, "Please enter the surname of the user")
       elsif self.affiliation.blank?
         self.errors.add(:affiliation, "Please enter the affiliation of the user")
       else
         generated_password = Devise.friendly_token.first(10)
-        _user = User.new(email:       self.email, 
-                         password:    generated_password,
-                         name:        self.name, 
-                         surname:     self.surname, 
-                         affiliation: self.affiliation,
-                         address:     self.address)
+        _user = User.new(email:        self.email, 
+                         password:     generated_password,
+                         name:         self.name, 
+                         surname:      self.surname, 
+                         affiliation:  self.affiliation,
+                         address:      self.address, 
+                         confirmed_at: Time.now)
         if _user.save
           RegistrationMailer.welcome(_user, generated_password, self).deliver
           self.user_id = _user.id 
