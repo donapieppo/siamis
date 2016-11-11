19994d5b33a3de345c9e270d1fe9ba91dc8de4aa
Donapieppo <donapieppo@yahoo.it>
2016-11-11 15:47:41 +0100

    plenaries

--- app/controllers/plenaries_controller.rb
+++ app/controllers/plenaries_controller.rb
@@ -1,4 +1,9 @@
 class PlenariesController < ConferenceSessionsController
+
+  def index
+    @plenaries = Plenary.includes(schedule: :room).order('schedules.start').all
+  end
+
   def new 
     @conference_session = Plenary.new
   end
@@ -6,7 +11,7 @@ class PlenariesController < ConferenceSessionsController
   def create 
     @conference_session = Plenary.new(conference_session_params)
     if @conference_session.save 
-      redirect_to plenaries_path, notice: 'The Plenary Invited Session has been created.'
+      redirect_to @conference_session, notice: 'The Plenary Invited Session has been created.'
     else
       render action: :new
     end
--- app/models/plenary.rb
+++ app/models/plenary.rb
@@ -1,2 +1,19 @@
+# In general many presnetations
+# In Siam is18 only one so we getr data from it
 class Plenary < ConferenceSession
+  after_create :add_relative_presentation
+
+  def presentation
+    @presentation ||= presentations.first
+  end
+
+  def speaker
+    self.presentation.authors.first
+  end
+
+  private
+
+  def add_relative_presentation
+    @presentation = self.presentations.create(name: self.name)
+  end
 end
