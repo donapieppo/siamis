0fbb7158926b1fed2bd2890157aa45afdfad3040
Donapieppo <donapieppo@yahoo.it>
2016-12-13 13:03:03 +0100

    author photo. Use in plenary

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,4 +6,11 @@ class Author < Role
       a.update_attribute(:speak, a == self)
     end
   end
+
+  def photo_asset
+    unless self.user.email.blank?
+      image_base_name = self.user.email.gsub(/@.*/, '').gsub(/[^0-9A-Za-z.\-]/, '_')
+      "speakers/#{image_base_name}.jpg"
+    end
+  end
 end
