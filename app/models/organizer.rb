5475a72d1388b52a6dde51b1ecfde0c2b15ee40f
Donapieppo <donapieppo@yahoo.it>
2016-12-20 13:44:35 +0100

    had forgotten

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -2,5 +2,5 @@ class Organizer < Role
   belongs_to :minisymposium, foreign_key: :conference_session_id, optional: true
   belongs_to :minitutorial,  foreign_key: :conference_session_id, optional: true
   belongs_to :conference_session, foreign_key: :conference_session_id, optional: true
-
+  belongs_to :user
 end
