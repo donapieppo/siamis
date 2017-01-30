6877e11c137c174e2071cc66a907a5b29ba8a721
Donapieppo <donapieppo@yahoo.it>
2017-01-30 13:42:24 +0100

    conference registration index for commettee

--- app/controllers/conference_registrations_controller.rb
+++ app/controllers/conference_registrations_controller.rb
@@ -1,11 +1,19 @@
 class ConferenceRegistrationsController < ApplicationController
+  before_action :user_in_organizer_commettee!, only: [:index]
+
+  def index
+    @conference_registrations = ConferenceRegistration.includes(:user, :payment)
+  end
+
   def new
     @me  = current_user
+    @fields = User.all_fields
     @fee = Fee.new(@me)
     @price_to_pay_and_reason = @fee.price_to_pay_and_reason
   end
 
   def show
+    @fields = User.all_fields
     @conference_registration = current_user.conference_registration
     @payment = @conference_registration.payment
   end
