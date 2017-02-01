c2de162659624fc7ab96eec244c61dc20c8f55a0
Donapieppo <donapieppo@yahoo.it>
2017-02-01 11:48:04 +0100

    moved

--- app/models/minisymposium.rb
+++ app/models/minisymposium.rb
@@ -1,6 +1,4 @@
 class Minisymposium < ConferenceSession
-  DURATION = 30
-
   has_many :presentations, foreign_key: :conference_session_id, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
