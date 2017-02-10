2d9db304117763fb4980b9fb8caf6e15c066711e
Donapieppo <donapieppo@yahoo.it>
2017-02-10 12:00:17 +0100

    correct redirect

--- app/controllers/plenaries_controller.rb
+++ app/controllers/plenaries_controller.rb
@@ -12,7 +12,7 @@ class PlenariesController < ConferenceSessionsController
   def create 
     @conference_session = Plenary.new(conference_session_params)
     if @conference_session.save 
-      redirect_to [:edit, @conference_session], notice: 'The Plenary Invited Session has been created.'
+      redirect_to new_presentation_author_path(@conference_session.presentation), notice: 'The Plenary Invited Session has been created. Please provide authors and chairs.'
     else
       render action: :new
     end
