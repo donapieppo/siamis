3ba6a39a14517080816c597eadc4c5f7f3144dac
Donapieppo <donapieppo@yahoo.it>
2017-02-15 10:50:59 +0100

    bug

--- app/helpers/panels_helper.rb
+++ app/helpers/panels_helper.rb
@@ -26,28 +26,28 @@ module PanelsHelper
     return unless current_user_owns?(what)
 
     capture do 
-    concat(link_to icon('pencil') + ' edit', [:edit, what], class: :button)
-      
-    # plenary, minitutorial
-    if what.respond_to?(:chairs) 
-      concat(link_to icon('plus') + ' add chair', [:new, what, :organizer], class: :button)
-    elsif what.is_a?(Minisymposium)
-      concat(link_to icon('plus') + ' add organizer', [:new, what, :organizer], class: :button)
-    end 
+      concat(link_to icon('pencil') + ' edit', [:edit, what], class: :button)
 
-    if what.respond_to?(:authors) 
-      concat(link_to icon('plus') + ' add author', [:new, what.respond_to?(:presentation) ? what.presentation : what, :author], class: :button)
-    end 
+      # plenary, minitutorial
+      if what.respond_to?(:chairs) 
+        concat(link_to icon('plus') + ' add chair', [:new, what, :organizer], class: :button)
+      elsif what.is_a?(Minisymposium)
+        concat(link_to icon('plus') + ' add organizer', [:new, what, :organizer], class: :button)
+      end 
 
-    if what.respond_to?(:presentations)
-      concat(link_to icon('file-audio-o') + ' add presentation', [:new, what, :presentation], class: :button)
-    end 
+      if what.respond_to?(:authors) 
+        concat(link_to icon('plus') + ' add author', [:new, what.respond_to?(:presentation) ? what.presentation : what, :author], class: :button)
+      end 
 
-    concat(link_to_delete('delete', what, button: true))
+      if what.respond_to?(:presentations)
+        concat(link_to icon('file-audio-o') + ' add presentation', [:new, what, :presentation], class: :button)
+      end 
 
-    if conference_session = what.try(:conference_session)   
-      concat(link_to icon('reply') + " back to #{conference_session.class}", conference_session)
-    end 
+      concat(link_to_delete('delete', what, button: true))
+
+      if conference_session = what.try(:conference_session)   
+        concat(link_to icon('reply') + " back to #{conference_session.class}", conference_session)
+      end 
     end
   end
 
@@ -55,33 +55,36 @@ module PanelsHelper
     return unless user_in_organizer_commettee?
 
     capture do 
-    unless what.is_a?(Presentation)
-      concat(link_to icon('clock-o') + ' schedule', [:new, what, :schedule], class: :button)
-    end 
+      unless what.is_a?(Presentation)
+        concat(link_to icon('clock-o') + ' schedule', [:new, what, :schedule], class: :button)
+      end 
 
-    if what.is_a?(Presentation) or what.is_a?(Minisymposium)
-      if what.accepted
-        concat(link_to icon('list')     + ' manage presentations', [:manage_presentations, what])
-        concat(link_to icon('circle-o') + ' refuse',               [:refuse, what], method: :put)
-      else 
-        concat(link_to icon('check') + ' accept', [:accept, what], method: :put)
+      if what.is_a?(ConferenceSession)
+        concat(link_to icon('list') + ' manage presentations', [:manage_presentations, what])
       end
-    end
+
+      if what.is_a?(Presentation) or what.is_a?(Minisymposium)
+        if what.accepted
+          concat(link_to icon('circle-o') + ' refuse',               [:refuse, what], method: :put)
+        else 
+          concat(link_to icon('check') + ' accept', [:accept, what], method: :put)
+        end
+      end
     end
   end
 
   def scientific_commettee_actions(what)
     capture do 
-    if user_in_scientific_commettee? and what.ratable?
-      concat(link_to icon('star') + ' rate', [:new, what, :rating], remote: true)
-    end 
+      if user_in_scientific_commettee? and what.ratable?
+        concat(link_to icon('star') + ' rate', [:new, what, :rating], remote: true)
+      end 
     end
   end
 
   # what = presentation or minisymposium
   def panel_actions(what)
-    owner_actions(what) 
-    scientific_commettee_actions(what) 
+    owner_actions(what) +
+    scientific_commettee_actions(what) +
     organizer_commettee_actions(what)
   end
 
