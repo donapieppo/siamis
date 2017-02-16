34cd5c74d334fac4ed88bcff0841cf7e4b88f8ac
Donapieppo <donapieppo@yahoo.it>
2017-02-16 16:37:48 +0100

    includes

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,12 +1,12 @@
 class SubmissionsController < ApplicationController
   def index
     if user_in_organizer_commettee?
-      @minisymposia  = Minisymposium.submitted.all
-      @presentations = Presentation.unassigned.submitted.not_poster
-      @posters       = Presentation.submitted.poster
+      @minisymposia  = Minisymposium.submitted.includes(ratings: :user, organizers: :user).all
+      @presentations = Presentation.unassigned.submitted.includes(ratings: :user, authors: :user).not_poster
+      @posters       = Presentation.submitted.includes(ratings: :user, authors: :user).poster
     else
-      @minisymposia  = current_user.minisymposia.all
-      @presentations = current_user.presentations.all
+      @minisymposia  = current_user.minisymposia.includes(organizers: :user).all
+      @presentations = current_user.presentations.includes(authors: :user).all
     end
   end
 end
