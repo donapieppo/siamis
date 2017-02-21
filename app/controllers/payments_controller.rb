cecb26088a34078756c1de3e87769dbddb69b00a
Donapieppo <donapieppo@yahoo.it>
2017-02-21 11:56:11 +0100

    BUG

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,7 +12,7 @@ class PaymentsController < ApplicationController
 
   # params[:payment][:single_day] is the number of day (from 0)
   def create
-    conference_day = params[:payment][:single_day] ? Schedule.conference_day(params[:payment][:single_day].to_i) : nil
+    conference_day = (params[:payment] and params[:payment][:single_day]) ? Schedule.conference_day(params[:payment][:single_day].to_i) : nil
     @fee = Fee.new(current_user, single_day: conference_day)
     @payment = current_user.payments.create(amount: @fee.price_to_pay, single_day: conference_day)
     redirect_to @payment.redirect_url
