dd9242aed5f1380882373978ff9213aa7b1289e7
Donapieppo <donapieppo@yahoo.it>
2017-02-22 15:37:39 +0100

    show all submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,9 @@
 class SubmissionsController < ApplicationController
   def index
     if user_in_organizer_commettee?
-      @minisymposia  = Minisymposium.submitted.includes(ratings: :user, organizers: :user).all
+      @minisymposia  = Minisymposium.includes(ratings: :user, organizers: :user).all
       @presentations = Presentation.unassigned.submitted.includes(ratings: :user, authors: :user).not_poster
-      @posters       = Presentation.submitted.includes(ratings: :user, authors: :user).poster
+      @posters       = Presentation.unassigned.submitted.includes(ratings: :user, authors: :user).poster
     else
       @minisymposia  = current_user.minisymposia.includes(organizers: :user).all
       @presentations = current_user.presentations.includes(authors: :user).all
