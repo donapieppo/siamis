0ad96486875ae96109716eef4a908eecbbde7dcc
Donapieppo <donapieppo@yahoo.it>
2017-03-08 09:29:11 +0100

    show one of three for admin

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,9 @@
 class SubmissionsController < ApplicationController
   def index
     if user_in_organizer_commettee?
-      @minisymposia  = Minisymposium.includes(ratings: :user, organizers: :user).all
-      @presentations = Presentation.unassigned.submitted.includes(ratings: :user, authors: :user).not_poster
-      @posters       = Presentation.unassigned.submitted.includes(ratings: :user, authors: :user).poster
+      @minisymposia  = params[:minisymposium] ? Minisymposium.includes(ratings: :user, organizers: :user).all : []
+      @presentations = params[:contributed] ? Presentation.unassigned.includes(ratings: :user, authors: :user).not_poster.all : []
+      @posters       = params[:poster] ? Presentation.unassigned.includes(ratings: :user, authors: :user).poster.all : []
     else
       @minisymposia  = current_user.minisymposia.includes(organizers: :user).all
       @presentations = current_user.presentations.includes(authors: :user).all
