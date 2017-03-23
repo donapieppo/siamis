f003d03b32df8354652f088d6b20c43834427ac9
Donapieppo <donapieppo@yahoo.it>
2017-03-23 16:44:32 +0100

    guest can see submissions instructions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,13 +1,14 @@
 class SubmissionsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   # all minisymposia
   # presentations only unaccepted
   def index
-    if user_in_organizer_committee?
-      @minisymposia  = params[:minisymposium] ? Minisymposium.includes(:schedule, ratings: :user, organizers: :user) : []
-      @unassigned_presentations = params[:contributed] ? Presentation.includes(:conference_session, ratings: :user, authors: :user).unassigned.not_poster : []
-      @posters       = params[:poster] ? Presentation.includes(:conference_session, ratings: :user, authors: :user).poster : []
-    else
+    if current_user and user_in_organizer_committee?
+      @minisymposia  = params[:minisymposium] ? Minisymposium.includes(:schedule, ratings: :user, organizers: :user) : nil
+      @unassigned_presentations = params[:contributed] ? Presentation.includes(:conference_session, ratings: :user, authors: :user).unassigned.not_poster : nil
+      @posters       = params[:poster] ? Presentation.includes(:conference_session, ratings: :user, authors: :user).unassigned.poster : nil
+    elsif current_user
       @minisymposia  = current_user.minisymposia.includes(organizers: :user)
       @presentations = current_user.presentations.includes(authors: :user)
     end
