a3854a22425b4803117f4658a1d40fc68538a7b0
Donapieppo <donapieppo@yahoo.it>
2017-03-24 16:11:17 +0100

    cookie policy

--- app/helpers/privacy_helper.rb
+++ app/helpers/privacy_helper.rb
@@ -5,12 +5,24 @@ module PrivacyHelper
 
   def privacy_alert
     name = privacy_cookie_name
-    if cookies[name] 
+    if ! cookies[name] 
       return ""
     else
       cookies[name] = { value: "accepted", expires: 1.year.from_now }
-      raw %Q|<div class="alert alert-warning alert-dismissible" role="alert">
-           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times; chiudi</span></button> Questo sito web utilizza cookie analitici di terze parti. Utilizzando il sito si intende accettata la Cookie Policy.</div>|
+      raw %Q|
+      <div class="alert alert-warning alert-dismissible" role="alert">
+         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+         <span aria-hidden="true">&times; close</span></button> #{PRIVACY_TEXT}
+      </div>|
     end
   end
 end
+
+
+PRIVACY_TEXT=%q|
+<p>A number of our pages use <strong>cookies</strong> to identify you when you sign-in to our site.
+<p>The cookie-related information is not used to identify you personally and the pattern data is fully under our control. These cookies are not used for any purpose other than those described here. <a href="http://www.unibo.it/en/university/privacy-policy-and-legal-notes/privacy-policy/general-information-on-the-processing-of-personal-data-in-the-university-portal-system-websites">Unibo policy</a></p>
+<p>By continuing to browse the site, or by clicking on “close”, you consent to the use of cookies.</p>
+|.html_safe
+
+
