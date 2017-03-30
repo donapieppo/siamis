d7cd5eeb23f0a34569ed144d92bbc887026a9318
Donapieppo <donapieppo@yahoo.it>
2017-03-30 09:03:33 +0200

    author order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @home_header    = true
+    @home_header   = true
+    @plenaries     = Plenary.includes(presentation: [authors: :user], schedule: :room).order('users.surname, users.name')
+    @minitutorials = Minitutorial.includes(presentation: [authors: :user], schedule: :room).order('users.surname, users.name')
   end
 
   def contacts
