c13bd802248e71be26feadbb8e36cbee1b170dc2
Donapieppo <donapieppo@yahoo.it>
2017-04-05 10:13:59 +0200

    test

--- spec/models/minitutorial_spec.rb
+++ spec/models/minitutorial_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Minitutorial, type: :model do
   let (:minitutorial) { FactoryGirl.create(:minitutorial) }
+  let (:user) { FactoryGirl.create(:user) }
 
   it "does create the presentation" do
     expect(minitutorial.presentation).to be
   end
 
-  it "has a presentation without name or abstract" do
-    expect(minitutorial.presentation.name).not_to be
-    expect(minitutorial.presentation.abstract).not_to be
+  it "has a presentation" do
+    expect(minitutorial.presentation).to be_a(Presentation)
   end
 
+  it "has a presentation with same name" do
+    expect(minitutorial.presentation.name).to eq(minitutorial.name)
+    # expect(minitutorial.presentation.abstract).to eq(minitutorial.description)
+  end
+
+  it "accepts user for organizer" do
+    expect(minitutorial.organizers.build(user: user)).to be_valid
+  end
 end
 
