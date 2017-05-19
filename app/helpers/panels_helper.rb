440e33e0742a0a2aef4ccfec6af4e73c01a11cc2
Donapieppo <donapieppo@yahoo.it>
2017-05-19 11:45:21 +0200

    ldquo

--- app/helpers/panels_helper.rb
+++ app/helpers/panels_helper.rb
@@ -11,9 +11,10 @@ module PanelsHelper
   def presentation_parent(presentation)
     presentation.conference_session or return ""
     content_tag(:p) do
-      concat "This presentation is part of " 
-      concat content_tag(:strong, h(t(presentation.conference_session.class)) + " ") 
+      concat "This presentation is part of "
+      concat content_tag(:strong, h(t(presentation.conference_session.class)) + " &ldquo;".html_safe) 
       concat content_tag(:em, link_to(presentation.conference_session, presentation.conference_session)) 
+      concat "&rdquo; ".html_safe
       concat "<br/>".html_safe 
       if presentation.conference_session.is_a?(Minisymposium) 
         concat content_tag(:small, "organized by: ".html_safe + show_roles(presentation.conference_session.organizers)) + "."
