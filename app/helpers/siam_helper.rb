7698291102af7e7c014d540f9ba7014a68628a97
Donapieppo <donapieppo@yahoo.it>
2017-05-19 16:00:13 +0200

    text

--- app/helpers/siam_helper.rb
+++ app/helpers/siam_helper.rb
@@ -1,8 +1,8 @@
 module SiamHelper
   def bologna_wecome_links
-    "visit <br/>".html_safe + 
+    "for more informations on Bologna please visit <br/>".html_safe + 
     link_to(image_tag('bologna_welcome.png'), 'http://www.bolognawelcome.com/en') + 
-    "<br/>or download the guide in pdf <br/>".html_safe +
-    link_to('Bologna, Your Destination', '/BolognaWelcome2017.pdf') 
+    "<br/>or download this guide in pdf <br/>".html_safe +
+    link_to('- Bologna, Your Destination -', '/BolognaWelcome2017.pdf') 
   end
 end
