9ffb08a351b987a4b3fc0cfb3f40bae87d56d5f2
Donapieppo <donapieppo@yahoo.it>
2017-06-01 15:22:54 +0200

    bug to think. workaround for 0 words

--- app/assets/javascripts/siam.js
+++ app/assets/javascripts/siam.js
@@ -1,5 +1,7 @@
 var update_wcount = function(el) {
-  var words = el.val().trim().split(/\s+/ ).length;
+  var words = el.val().trim().split(/\s+/).length;
+  // boh 
+  if (el.val().trim() == "") { words = 0 };
   $('#wcount').html(words);
 };
 
