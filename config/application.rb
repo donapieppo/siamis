008a188d423d1162f50c148113f00b32855e2d9d
Donapieppo <donapieppo@yahoo.it>
2017-06-01 15:22:05 +0200

    rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Siamis
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     config.time_zone = 'Rome'
     config.i18n.default_locale = :en
     config.active_record.time_zone_aware_types = [:datetime, :time]
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
