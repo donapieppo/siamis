a3dbf8ff62213c21dc427fdfb56f7828298c4a47
Donapieppo <donapieppo@yahoo.it>
2017-06-08 11:33:15 +0200

    typos informationS ExHibits

--- app/helpers/siam_helper.rb
+++ app/helpers/siam_helper.rb
@@ -1,6 +1,6 @@
 module SiamHelper
   def bologna_wecome_links
-    "for more informations on Bologna please visit <br/>".html_safe + 
+    "for more information on Bologna please visit <br/>".html_safe + 
     link_to(image_tag('bologna_welcome.png'), 'http://www.bolognawelcome.com/en') + 
     "<br/>or download this guide in pdf <br/>".html_safe +
     link_to('- Bologna, Your Destination -', '/BolognaWelcome2017.pdf') 
