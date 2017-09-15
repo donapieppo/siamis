5939f1536b5f0f151f13e08af66ce5a7d73d071f
Donapieppo <donapieppo@yahoo.it>
2017-09-15 11:03:54 +0200

    scope for global tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,11 +5,27 @@ class Tag < ApplicationRecord
 
   validates_uniqueness_of :name
 
+  scope :global,     -> { where(global: true) }
+  scope :not_global, -> { where('global = 0 or global IS NULL') }
+
   def to_s
      name
   end
 
   def self.all_tags
     Tag.order(:name).select(:name).map(&:name).join(', ')
+  end
+
+  # ????? downcase! Downcases the contents of str, returning nil if no changes were made. 
+  def self.add_manual_tags(tags_string)
+    tags_string.split(',').map do |ts|
+      t = ts.downcase.strip
+      if existent_tag = Tag.where(name: t).first
+        existent_tag
+      else
+        logger.info("Create tag #{t}")
+        Tag.create(name: t)
+      end
+    end
   end
 end
