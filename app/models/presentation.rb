eb1ffab03305e34fa2cbcee3c221e06097deba9a
Donapieppo <donapieppo@yahoo.it>
2017-10-05 18:03:09 +0200

    touch conference_session on change

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -4,7 +4,7 @@ class Presentation < ApplicationRecord
   # has_many :users, through: :authors
   has_many :ratings, dependent: :destroy
   has_many :interests, dependent: :destroy
-  belongs_to :conference_session, optional: true
+  belongs_to :conference_session, optional: true, touch: true
   belongs_to :minisymposium, foreign_key: :conference_session_id, optional: true
   belongs_to :minitutorial,  foreign_key: :conference_session_id, optional: true
   belongs_to :plenary,       foreign_key: :conference_session_id, optional: true
