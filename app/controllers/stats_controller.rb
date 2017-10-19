c372700e9e511a46c2e1ed853b907d0e9a175d2c
Donapieppo <donapieppo@yahoo.it>
2017-10-19 17:03:36 +0200

    nationalities

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,9 @@ class StatsController < ApplicationController
   before_action :user_in_organizer_committee_or_cochair!
 
   def countries
-    @users = User.order(:country).group(:country).count(:country)
+    @users      = User.order(:country).group(:country).count('users.id')
+    @speakers   = Author.includes(:user).where('speak = 1').group('users.country').count('users.id')
+    @organizers = Organizer.includes(:user).group('users.country').count('users.id')
   end
 end
 
