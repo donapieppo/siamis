e34e58e12192c44c2af66c063e31ee94a2a5c5ae
Donapieppo <donapieppo@yahoo.it>
2017-11-06 09:30:37 +0100

    all can see

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,5 @@
 class StatsController < ApplicationController
-  before_action :user_in_organizer_committee_or_cochair!
+  # before_action :user_in_organizer_committee_or_cochair!
 
   def countries
     @users      = User.order(:country).group(:country).count('users.id')
