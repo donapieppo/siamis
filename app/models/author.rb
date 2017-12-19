4c91882c47b3780674dc26264e2d7b4dfedf18a7
Donapieppo <donapieppo@yahoo.it>
2017-12-19 12:01:10 +0100

    scope :speaker

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,8 @@
 class Author < Role
   belongs_to :presentation
 
+  scope :speaker, -> { where(speak: true) }
+
   def only_speaker!
     self.presentation.authors.each do |a|
       a.update_attribute(:speak, a == self)
