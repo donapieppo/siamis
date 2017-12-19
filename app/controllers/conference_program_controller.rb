8c5617b1f2724c0cc0c86cf4719f3d5247e5783c
Donapieppo <donapieppo@yahoo.it>
2017-12-19 11:58:54 +0100

    temporary

--- app/controllers/conference_program_controller.rb
+++ app/controllers/conference_program_controller.rb
@@ -2,6 +2,21 @@ class ConferenceProgramController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    if user_in_organizer_or_scientific_committee?
+      # always 
+      @daynumber = params[:day]
+      @day = Schedule.conference_day(@daynumber)
+      @room = Room.find(params[:room_id]) if params[:room_id]
+      @conference_program = Schedule.day_program(@day, @room)
+    end
+  end
+
+  def index2
+    if user_in_organizer_or_scientific_committee?
+      # always 
+      @day = Schedule.conference_day(params[:day])
+      @conference_program = Schedule.day_program(@day)
+    end
   end
 end
 
