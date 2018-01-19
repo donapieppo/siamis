ee7aac7e8ace221a9fb307e1ec52aef4e906841c
Donapieppo <donapieppo@yahoo.it>
2018-01-19 11:45:49 +0100

    all users can see + cache

--- app/controllers/conference_program_controller.rb
+++ app/controllers/conference_program_controller.rb
@@ -2,17 +2,18 @@ class ConferenceProgramController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if user_in_organizer_or_scientific_committee?
+    if current_user # user_in_organizer_or_scientific_committee?
       # always 
       @daynumber = params[:day]
       @day = Schedule.conference_day(@daynumber)
       @room = Room.find(params[:room_id]) if params[:room_id]
+
       @conference_program = Schedule.day_program(@day, room: @room)
     end
   end
 
   def print
-    if user_in_organizer_or_scientific_committee?
+    if current_user # user_in_organizer_or_scientific_committee?
       pdf = PrintableProgram.new()
       send_data pdf.render, filename: "program.pdf", type: "application/pdf"
     else
