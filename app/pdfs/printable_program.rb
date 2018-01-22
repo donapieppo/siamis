8f5e3bf5feeb62e2e5c6c9e878e84d8118d75760
Donapieppo <donapieppo@yahoo.it>
2018-01-22 11:19:06 +0100

    new try

--- app/pdfs/printable_program.rb
+++ app/pdfs/printable_program.rb
@@ -10,12 +10,43 @@ class PrintableProgram < Prawn::Document
     font "LiberationSans"
     font_size 6
 
+    old_hour_morning = nil
+
+    first_page
 
     Schedule.conference_days_array.each do |day|
       Schedule.startings_cest(day).each do |hour|
+        if (old_hour_morning != (old_hour_morning = (hour.split(':')[0].to_i < 12) ? true : false))
+          start_new_page
+        else
+          move_down 20
+        end
         slot(day, hour)
-        start_new_page
       end
+    end
+  end
+
+  def first_page
+    move_down 80
+    text " SIAM Conference on IMAGING SCIENCE", size: 32, align: :center
+    move_down 20
+    text "June 5-8, 2018", size: 28, align: :center
+    move_down 20
+    text "Bologna - Italy", size: 28, align: :center
+    move_down 60
+    text "parallel sessions", style: :bold, size: 26, align: :center
+    move_down 180
+    text "Green cells are for organizers/chairs while grey cells are for speakers", size: 18
+  end
+
+  def conference_session_color(cs)
+    case cs
+    when Minisymposium
+      '034f84'
+    when ContributedSession
+      'c94c4c'
+    else
+      '000000'
     end
   end
 
@@ -23,15 +54,19 @@ class PrintableProgram < Prawn::Document
     schedule = Schedule.day_program(day, start: hour).to_a
     schedule.any? or return 
 
+    # IP are alone
+    schedule.count < 2 and return ""
+
     res    = []
     colors = []
 
     colors << "ffffff"
-    res << schedule.map{|s| s.room.name }
+    res << schedule.map{|s| "#{s.room.building.name}" }
     colors << "ffffff"
-    res << schedule.map{|s| s.room.capacity }
-    colors << "f18f01"
-    res << schedule.map{|s| "<b>#{s.conference_session_with_part}</b>"}
+    res << schedule.map{|s| "#{s.room.name} (#{s.room.capacity})" }
+    colors << "ffffff"
+    # colors << "f18f01"
+    res << schedule.map{|s| cs = s.conference_session; "<color rgb='#{conference_session_color(cs)}'><b>#{cs.code_with_part(s.part)}\n#{cs.name}</b></color>"}
 
     # 0 = ----
     # 1 = --
@@ -39,7 +74,7 @@ class PrintableProgram < Prawn::Document
     organizers = []
     max = 0
     schedule.each do |s| 
-      session_organizers = s.conference_session.organizers.includes(:user).map(&:to_s)
+      session_organizers = s.conference_session.organizers.includes(:user).map(&:cn)
       max = session_organizers.size if max < session_organizers.size
       organizers << session_organizers
     end
@@ -56,7 +91,7 @@ class PrintableProgram < Prawn::Document
     speakers = []
     max = 0 
     schedule.each do |s| 
-      session_speakers = s.conference_session.speakers(s.part).map(&:to_s)
+      session_speakers = s.conference_session.speakers(s.part).map(&:cn)
       max = session_speakers.size if max < session_speakers.size
       speakers << session_speakers
     end
