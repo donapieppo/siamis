d0eeb9de5aa7b2a1b2171d5c9a56e3462c11d793
Donapieppo <donapieppo@yahoo.it>
2018-02-20 08:54:52 +0100

    new classes for program

--- app/helpers/style_helper.rb
+++ app/helpers/style_helper.rb
@@ -1,11 +1,11 @@
 module StyleHelper
   def show_schedule_session(schedule)
     cs = schedule.conference_session
-    content_tag(:div, class: 'row') do 
-      content_tag(:div, class: 'col-xs-3 col-md-2 label label-warning', style: 'font-size: 16px; font-weight: bold; padding-top: 1em; height: 3em') do
-        link_to cs.code_with_part(schedule.part), conference_session_path(schedule.conference_session, part: schedule.part), remote: true
+    content_tag(:div, class: 'row conference_program_item') do 
+      content_tag(:div, class: "#{conference_session_css_class(cs)} conference_program_item_name") do
+        link_to cs.code_with_part(schedule.part), conference_session_path(cs, part: schedule.part), remote: true
       end +
-      content_tag(:div, class: 'col-xs-9 col-md-10') do
+      content_tag(:div, class: 'conference_program_item_description') do
         link_to(conference_session_path(schedule.conference_session, part: schedule.part), remote: true) { cs.name } + "<br/>".html_safe +
         "<small>#{h schedule.room}</small>".html_safe 
       end
