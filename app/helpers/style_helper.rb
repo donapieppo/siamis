026a4af68fc1b25b1227d9113428293a51ae1e4c
Donapieppo <donapieppo@yahoo.it>
2018-02-23 11:08:33 +0100

    old

--- app/helpers/style_helper.rb
+++ app/helpers/style_helper.rb
@@ -2,8 +2,10 @@ module StyleHelper
   def show_schedule_session(schedule)
     cs = schedule.conference_session
     content_tag(:div, class: 'row conference_program_item') do 
-      content_tag(:div, class: "#{conference_session_css_class(cs)} conference_program_item_name") do
-        link_to cs.code_with_part(schedule.part), conference_session_path(cs, part: schedule.part), remote: true
+      link_to(conference_session_path(cs, part: schedule.part), remote: true) do
+        content_tag(:div, class: "#{conference_session_css_class(cs)} conference_program_item_name") do
+          cs.code_with_part(schedule.part)
+        end 
       end +
       content_tag(:div, class: 'conference_program_item_description') do
         link_to(conference_session_path(schedule.conference_session, part: schedule.part), remote: true) { cs.name } + "<br/>".html_safe +
