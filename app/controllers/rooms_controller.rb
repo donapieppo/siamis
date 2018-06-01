76524b15222905e77cde6f3662049578d6f7429e
Donapieppo <donapieppo@yahoo.it>
2018-06-01 09:04:22 +0200

    skip auth for show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :user_in_organizer_committee!, except: [:show]
   before_action :set_room, only: [:show, :edit, :update, :destroy]
 
